532c7da8b0cd8f02728bc73a08bb633355e552f4
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-06 20:45:12 -0300

    [NEW] Adding DbContext class, connection string and setting in main app the dbcontext and swagger configs.

---
+++ WeatherAppAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAppAPI.Model;
+
+namespace WeatherAppAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) {}
+
+        public DbSet<FavoriteCity> FavoriteCity { get; set; }
+    }
+}
--- WeatherAppAPI/obj/Debug/net9.0/WeatherAppAPI.AssemblyInfo.cs
+++ WeatherAppAPI/obj/Debug/net9.0/WeatherAppAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherAppAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f5815d41528590881bbf88a0c450bfaebc3844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1940150635330d9db9bc5b9e77d007e3a1570d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherAppAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherAppAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
