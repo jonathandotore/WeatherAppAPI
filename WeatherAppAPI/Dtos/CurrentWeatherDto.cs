f9d3685921f7c09bc2ca5ff107a0f3d782e4b44b
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-07 00:22:28 -0300

    [NEW] Adding DTO to receive and deserialize json response from OpenWeatherApi

---
+++ WeatherAppAPI/Dtos/CurrentWeatherDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WeatherAppAPI.Dtos
+{
+    public class CurrentWeatherDto
+    {
+        public string CityName { get; set; }
+        public float Temperature { get; set; }
+        public float MinTemperature { get; set; }
+        public float MaxTemperature { get; set; }
+        public string Condition { get; set; }
+        public int Humidity { get; set; }
+    }
+}
+    
