9c7f5ba9437567fee9a59f0c4c76bf0d627a74a5
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-07 15:25:54 -0300

    [NEW] Adding a new endpoint to get current city weather.

--- WeatherAppAPI/Controllers/WeatherController.cs
+++ WeatherAppAPI/Controllers/WeatherController.cs
@@ -15,18 +15,39 @@ namespace WeatherAppAPI.Controllers
         public WeatherController(IWeatherService weatherService)
          => _weatherService = weatherService;
 
+
+        /// <summary>
+        /// Returns a weahter forecast for the current city
+        /// </summary>
+        /// <param name="city">City Name</param>
+        /// <returns>Weather forescast for current city</returns>
+        [HttpGet("currentcity")]
+        public async Task<ActionResult<ServiceResponse<CurrentWeatherDto>>> GetCurrentWeatherAsync(string city)
+        {
+            if (string.IsNullOrEmpty(city))
+                return BadRequest("Your search cannot be completed, city name is required!");
+
+            var currentForecast = await _weatherService.GetCurrentWeatherAsync(city);
+
+            if (currentForecast == null)
+                return NotFound($"Not found");
+
+            return Ok(currentForecast);
+        }
+
+
         /// <summary>
         /// Returns a weahter forecast for the next five days
         /// </summary>
         /// <param name="city">City Name</param>
         /// <returns>Weather forescast list by day</returns>
         [HttpGet("weatherforecastfivedays/{city}")]
-        public async Task<ActionResult<ServiceResponse<List<WeatherAllDayDto>>>> GetForecastWeatherForFiveDays(string city)
+        public async Task<ActionResult<ServiceResponse<List<WeatherAllDayDto>>>> GetForecastWeatherForFiveDaysAsync(string city)
         {
             if (string.IsNullOrEmpty(city))
                 return BadRequest("Your search cannot be completed, city name is required!");
 
-            var forecasts = await _weatherService.GetForecastWeatherForFiveDays(city);
+            var forecasts = await _weatherService.GetForecastWeatherForFiveDaysAsync(city);
 
             if (forecasts == null)
                 return NotFound($"Not found");
