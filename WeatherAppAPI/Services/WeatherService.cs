0fe949f37647c581777e3b8e23e026c20dcb7fd4
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-07 14:50:50 -0300

    [UPD] Refact GetWeatherAsync and including GetForecastWeatherForFiveDays method on Services

--- WeatherAppAPI/Services/WeatherService.cs
+++ WeatherAppAPI/Services/WeatherService.cs
@@ -3,6 +3,8 @@ using System.Text.Json;
 using WeatherAppAPI.Dtos;
 using WeatherAppAPI.Interfaces;
 using WeatherAppAPI.Model;
+using static WeatherAppAPI.Model.CurrentDayWeatherResponse;
+using static WeatherAppAPI.Model.OpenWeatherResponse;
 
 namespace WeatherAppAPI.Services
 {
@@ -21,20 +23,33 @@ namespace WeatherAppAPI.Services
         {
             string apiKey = _config["OpenWeather:ApiKey"];
             string url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric&lang=pt_br";
-            ServiceResponse<CurrentWeatherDto> response = new();
+            ServiceResponse<CurrentWeatherDto> response = new() { Data = null };
 
             try
             {
                 var resp = await _httpClient.GetAsync(url);
                 if (!resp.IsSuccessStatusCode)
-                    return null;
+                    return response;
 
                 var json = await resp.Content.ReadAsStringAsync();
-                var weatherDto = JsonConvert.DeserializeObject<CurrentWeatherDto>(json);
+                var weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(json);
 
-                response.Data = weatherDto;
+                if (weatherResponse?.Main == null)
+                    return response;
+
+                response.Data = new CurrentWeatherDto()
+                {
+                    CityName = weatherResponse.Name,
+                    Temperature = weatherResponse.Main.Temp,
+                    MinTemperature = weatherResponse.Main.TempMin,
+                    MaxTemperature = weatherResponse.Main.TempMax,
+                    Condition = weatherResponse.Weather.First().Description,
+                    Humidity = weatherResponse.Main.Humidity,
+                    Icon = weatherResponse.Weather.First().Icon
+                };
+                
                 response.Message = "City weather succesfully found!";
-                response.Status = false;
+                response.Status = true;
 
                 return response;
 
@@ -49,5 +64,62 @@ namespace WeatherAppAPI.Services
             }
 
         }
+
+        public async Task<ServiceResponse<List<WeatherAllDayDto>>> GetForecastWeatherForFiveDays(string cityName)
+        {
+            string apiKey = _config["OpenWeather:ApiKey"];
+            string url = $"https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={apiKey}&units=metric&lang=pt_br";
+            ServiceResponse<List<WeatherAllDayDto>> response = new();
+
+            try
+            {
+                var resp = await _httpClient.GetAsync(url);
+                if (!resp.IsSuccessStatusCode)
+                    return new ServiceResponse<List<WeatherAllDayDto>>() { Data = null };
+
+                var json = await resp.Content.ReadAsStringAsync();
+                var weatherForecastAllDay = JsonConvert.DeserializeObject<ForecastRoot>(json);
+
+
+                if (weatherForecastAllDay?.List == null)
+                    return new ServiceResponse<List<WeatherAllDayDto>> { Data = null };
+
+                var groupedByDay = weatherForecastAllDay.List
+                                    .GroupBy(x => DateTime.Parse(x.DtTxt).Date)
+                                    .Take(5);
+
+                var result = new List<WeatherAllDayDto>();
+
+                foreach (var day in groupedByDay)
+                {
+                    double minTemperature = day.Min(t => t.MainForecast.MinTemperature);
+                    double maxTemperature = day.Max(t => t.MainForecast.MaxTemperature);
+                    string condition = day.First().Weather.First().Description;
+
+                    result.Add(new WeatherAllDayDto()
+                    {
+                        Date = day.Key,
+                        MinTemperature = minTemperature,
+                        MaxTemperature = maxTemperature,
+                        Condition = condition
+                    });
+                }
+
+                response.Data = result;
+                response.Message = "Forecast succesfully found!";
+                response.Status = true;
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Data = null;
+                response.Message = ex.Message;
+                response.Status = false;
+
+                return response;
+            }
+        }
     }
 }
+ 
