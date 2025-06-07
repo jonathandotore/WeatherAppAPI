0ba90b14a534e8742f4e5ba52d9ccbc3b78faad9
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-07 00:23:29 -0300

    [NEW] Service to get CurrentWeather searching by city name

---
+++ WeatherAppAPI/Services/WeatherService.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+using WeatherAppAPI.Dtos;
+using WeatherAppAPI.Interfaces;
+using WeatherAppAPI.Model;
+
+namespace WeatherAppAPI.Services
+{
+    public class WeatherService : IWeatherService
+    {
+        readonly HttpClient _httpClient;
+        readonly IConfiguration _config;
+
+        public WeatherService(HttpClient httpClient, IConfiguration config)
+        {
+            _httpClient = httpClient;
+            _config = config;
+        }
+
+        public async Task<ServiceResponse<CurrentWeatherDto>> GetWeatherAsync(string cityName)
+        {
+            string apiKey = _config["OpenWeather:ApiKey"];
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric&lang=pt_br";
+            ServiceResponse<CurrentWeatherDto> response = new();
+
+            try
+            {
+                var resp = await _httpClient.GetAsync(url);
+                if (!resp.IsSuccessStatusCode)
+                    return null;
+
+                var json = await resp.Content.ReadAsStringAsync();
+                var weatherDto = JsonConvert.DeserializeObject<CurrentWeatherDto>(json);
+
+                response.Data = weatherDto;
+                response.Message = "City weather succesfully found!";
+                response.Status = false;
+
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                response.Data = null;
+                response.Message = ex.Message;
+                response.Status = false;
+
+                return response;
+            }
+
+        }
+    }
+}
