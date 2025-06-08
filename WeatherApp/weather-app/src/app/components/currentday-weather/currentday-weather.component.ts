0c80f8945545580511f4d4166e9442b7ce68c673
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-08 17:23:38 -0300

    [UPD] Updating main components to "listening" other component (search) and react to user input search

--- WeatherApp/weather-app/src/app/components/currentday-weather/currentday-weather.component.ts
+++ WeatherApp/weather-app/src/app/components/currentday-weather/currentday-weather.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CurrentWeatherDto } from '../../../models/current-weather';
 import { WeatherService } from '../../services/weather.service';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { SearchCityService } from '../../shared/search-city.service';
 
 @Component({
   selector: 'app-currentday-weather',
@@ -11,30 +13,60 @@ import { CommonModule } from '@angular/common';
   styleUrl: './currentday-weather.component.scss'
 })
 
-export class CurrentdayWeatherComponent implements OnInit {
+export class CurrentdayWeatherComponent implements OnInit, OnDestroy {
   
   forecast: CurrentWeatherDto = {} as CurrentWeatherDto;
-  city: string = 'São Paulo';
+  city: string = 'São José do Rio Preto';
   loading: boolean = false;
   error: string = '';
 
-  constructor(private weatherService: WeatherService) {}
+  private citySubscription!: Subscription;
+  private apiCallSubscription!: Subscription;
+
+  constructor(
+    private weatherService: WeatherService,
+    private searchCityService: SearchCityService) {}
   
   ngOnInit(): void {
+    //Carrega a cidade padrão
     this.getForecast();
+
+    //Recebe as mudanças na cidade pesquisada
+    this.citySubscription = this.searchCityService.city$.subscribe(newCity => {
+      if (newCity && newCity.trim() !== '' && newCity.trim().toLowerCase() !== this.city.toLowerCase()) {
+        this.city = newCity.trim();
+        this.getForecast();
+      }
+    });
   }
 
   getForecast(): void {
     this.loading = true;
-    this.weatherService.getCurrentWeatherAsync(this.city).subscribe({
+    
+    if (this.apiCallSubscription) 
+      this.apiCallSubscription.unsubscribe();
+
+    this.apiCallSubscription = this.weatherService.getCurrentWeatherAsync(this.city).subscribe({
       next: (data) => {
         this.forecast = data;
         this.loading = false;
+        console.log(`Clima atual para ${this.city}:`, data)
       },
-      error: () => {
+      error: (err) => {
+        console.error(`Erro ao buscar clima para ${this.city}:`, err);
         this.error = "Erro ao tentar a busca na cidade solicitada. Verifique a cidade e tente novamente";
         this.loading = false;
+        this.forecast!;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    // Desinscrever para evitar vazamento de memória
+    if (this.citySubscription)
+      this.citySubscription.unsubscribe();
+
+    if (this.apiCallSubscription)
+      this.apiCallSubscription.unsubscribe();
   }
 }
--- WeatherApp/weather-app/src/app/components/five-days-weather/five-days-weather.component.ts
+++ WeatherApp/weather-app/src/app/components/five-days-weather/five-days-weather.component.ts
@@ -1,40 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FiveDaysWeatherDto } from '../../../models/five-days-weather-dto';
 import { WeatherService } from '../../services/weather.service';
 import { CommonModule } from '@angular/common';
+import { SearchCityService } from '../../shared/search-city.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-five-days-weather',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './five-days-weather.component.html',
-  styleUrl: './five-days-weather.component.scss'
 })
 
-export class FiveDaysWeatherComponent implements OnInit {
+export class FiveDaysWeatherComponent implements OnInit, OnDestroy {
 
   forecast: FiveDaysWeatherDto[] = [];
   city: string = 'São José do Rio Preto';
   loading: boolean = false;
   error: string = '';
 
-  constructor(private weatherService: WeatherService) {}
+  private citySubscription!: Subscription;
+  private apiCallSubscription: Subscription | null = null;
+  
+  constructor(
+    private weatherService: WeatherService,
+    private searchCityService: SearchCityService) {}
 
   ngOnInit(): void {
     this.getForecast();
+
+    this.citySubscription = this.searchCityService.city$.subscribe(newCity => {
+      if (newCity && newCity.trim() !== '' && newCity.trim().toLowerCase() !== this.city.toLowerCase()) {
+        this.city = newCity.trim();
+        this.getForecast();
+      }
+    });
   }
 
   getForecast(): void {
+    
+    this.error = '';
+    this.forecast!;
     this.loading = true;
-    this.weatherService.getFiveDaysForecast(this.city).subscribe({
+    
+    if (this.apiCallSubscription)
+      this.apiCallSubscription.unsubscribe();
+
+    this.apiCallSubscription = this.weatherService.getFiveDaysForecast(this.city).subscribe({
       next: (data) => {
         this.forecast = data;
         this.loading = false;
+        console.log(`Previsão de 5 dias para ${this.city}:`, data);
       },
-      error: () => {
+      error: (err) => {
+        console.error(`Erro ao buscar previsão para ${this.city}:`, err);
         this.error = 'Erro ao carregar a previsão do tempo para cinco dias.';
         this.loading = false;
       }
     });
+  }
+
+  ngOnDestroy(): void {
+    if (this.citySubscription) 
+      this.citySubscription.unsubscribe();
+    
+    if (this.apiCallSubscription) 
+      this.apiCallSubscription.unsubscribe();
   }
 }
