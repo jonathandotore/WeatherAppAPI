6b5769e8840a6e835f58171e35f15900ebcf5188
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-08 17:18:34 -0300

    [NEW] New search component for user input.

--- WeatherApp/weather-app/src/app/app.config.ts
+++ WeatherApp/weather-app/src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)]
+    provideRouter(routes),
+    importProvidersFrom(ReactiveFormsModule)
+  ]
 };
