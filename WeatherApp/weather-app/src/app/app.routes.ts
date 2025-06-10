aa322be2d6178133ae7b999f69279fc197092713
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-09 21:55:45 -0300

    [UPD] Setting up new route paths, importing routerlink and some stuffs on MAIN.TS

--- WeatherApp/weather-app/src/app/app.routes.ts
+++ WeatherApp/weather-app/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { FavoriteCitiesComponent } from './pages/favorite-cities/favorite-cities.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, title: 'ClimaApp'},
     { path: 'cidadesfavoritas', component: FavoriteCitiesComponent, title: 'Cidades favoritas'},
-    { path: '**', redirectTo: '' }
+    { path: 'login', component: LoginComponent, title: 'Efetuar login'},
+    { path: 'register', component: RegisterComponent, title: 'Efetuar cadastro'},
+    { path: '**', redirectTo: '' },
 ];
--- WeatherApp/weather-app/src/main.ts
+++ WeatherApp/weather-app/src/main.ts
@@ -1,13 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  // Use o spread operator para combinar os providers existentes de appConfig
-  // com os novos providers
   providers: [
-    ...(appConfig.providers || []), // Garante que providers de appConfig sejam incluídos
-    provideHttpClient()
+    ...(appConfig.providers || []),
   ]
 });
