2fa798eb1243b25d7cee79379abc5f705d0ab40d
Jonathan Dotore <jonathandotore@outlook.com>
2025-06-09 21:59:58 -0300

    [UPD] Setting up JWT interceptor on Main application

--- WeatherApp/weather-app/src/app/app.config.ts
+++ WeatherApp/weather-app/src/app/app.config.ts
@@ -3,11 +3,17 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'; 
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(ReactiveFormsModule)
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    }
   ]
 };
